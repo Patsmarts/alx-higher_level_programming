ee092eb59d15f870a1584ebb5bc8464189796955
Patrick <patrickpeterudo@gmail.com>
2022-12-09 13:50:58 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-if matrix is None:
-return
-else:
-new_matrix = []
-for i in matrix:
-newlist = list(map((lambda x: x * x), i))
-new_matrix.append(newlist)
-return (new_matrix)
+
+m = [list(map(lambda k: k * k, x)) for x in matrix]
+
+return (m)
